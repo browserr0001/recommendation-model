0f603fcda3607761e52c0c18701cdf15a9f7e749
shenghuh <shenghuh@andrew.cmu.edu>
2025-11-14 03:31:10 -0500

    Hot reloading changed pkl model

--- app/model_loader.py
+++ app/model_loader.py
@@ -1,5 +1,4 @@
-import os
-import pickle
+import os, pickle, threading
 from app.model.src.content_based import ContentBasedRecommender
 
 class ForceImportUnpickler(pickle.Unpickler):
@@ -9,9 +8,27 @@ class ForceImportUnpickler(pickle.Unpickler):
             return ContentBasedRecommender
         return super().find_class(module, name)
 
+_model_lock = threading.RLock()
+_current_model = None
+_current_path = None
+
+def load_model_from_path(path: str):
+    with open(path, "rb") as f:
+        return ForceImportUnpickler(f).load()
+
+def set_current_model(model, path):
+    global _current_model, _current_path
+    with _model_lock:
+        _current_model = model
+        _current_path = path
+
+def get_current_model_and_path():
+    with _model_lock:
+        return _current_model, _current_path
+
 def get_model(tag="Updated_Model") -> ContentBasedRecommender:
     print(f"loading model with tag: {tag}...")
     model_path = os.path.join(os.path.dirname(__file__), 'pkl_models', f'content_based_model_{tag}.pkl')
-    with open(model_path, 'rb') as f:
-        model = ForceImportUnpickler(f).load()
+    model = load_model_from_path(model_path)
+    set_current_model(model, model_path)
     return model
